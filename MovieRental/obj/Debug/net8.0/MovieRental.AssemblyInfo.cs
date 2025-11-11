8c647706a64a1e444d328d769c5f487dcf663882
Adriano Maia <E1293@itsector.pt>
2025-11-11 16:31:19 +0000

    Change README to answer questions asked

--- MovieRental/obj/Debug/net8.0/MovieRental.AssemblyInfo.cs
+++ MovieRental/obj/Debug/net8.0/MovieRental.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieRental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ee467d4fbd08377ea786cf71f5561fa257b73e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f56983a85ef8c0d18b40e518eafba15696b45e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieRental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieRental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
